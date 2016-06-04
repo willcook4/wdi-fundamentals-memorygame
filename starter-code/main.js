b67ef2ee9d577f5ad6f452bd8c35e3ef13123495
Will Cook <nzwillcook@gmail.com>
2016-06-04 14:11:15 +0100

    First logic game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,6 @@
-console.log("JS file is connected to HTML! Woo!")
+//console.log("Will, Your JS file is connected to your HTML file! WooHoo!")
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";

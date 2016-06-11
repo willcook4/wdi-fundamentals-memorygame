7d6e7f508a60f9f0a4dfca06ed509545b3de7e98
Will Cook <nzwillcook@gmail.com>
2016-06-11 13:16:16 +0100

    cards are now made within js file - Unit 10 assignment done

--- starter-code/Superceeded/main_before_assignment_10_backup.js
+++ starter-code/Superceeded/main_before_assignment_10_backup.js
@@ -1,27 +1,11 @@
-var gameBoard = document.getElementById('game-board');
-
-// Now Redundant
-//for (var i=0; i<gameBoard.length; i++) {
-//}
-
-function createBoard () {
-	for (var i=0; i<4; i++) {
-		var newCard = document.createElement('div');
-		newCard.className = 'card';
-		gameBoard.appendChild(newCard);
-	}
-};
-
-createBoard();
-
-
 //console.log("Will, Your JS file is connected to your HTML file! WooHoo!")
-/*
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/* Commented out from Unit 10 Assignment
 if (cardTwo === cardFour) {
 	//queen and king
 	alert("Sorry, try agian");
@@ -36,3 +20,4 @@ else if (cardThree === cardFour) {
 	alert("You got a match! 2 Kings");
 }
 */
+

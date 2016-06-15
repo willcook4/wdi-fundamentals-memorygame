9ef9529802de4f417f04695beccac5b540c1623b
Will Cook <nzwillcook@gmail.com>
2016-06-15 13:39:39 +0100

    cleaned up the code and added comments on logic

--- wills-code-upgrade/main.js
+++ wills-code-upgrade/main.js
@@ -1,8 +1,9 @@
 // Will cook WDI Fundamentals .js file 2016
+// Will's playaround version
 
 // Setup the variables...
 var gameBoard = document.getElementById('game-board');
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = ['queenH', 'queenC','queenD' ,'queenS','kingH', 'kingS', 'kingD', 'kingC'];
 var cardsInPlay = [];
 
 
@@ -19,16 +20,15 @@ function createBoard () {
 
 // Check if the 2 cards are the same type...
 var isMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match (2x cards the same)");
+	if (cardsInPlay[0][0] === cardsInPlay[1][0]) {
+		alert("You found a match (2x cards the same kind)");
 	} else {
 		alert("Not a match, try again!");
 		// Moved to isTwoCards 
 		//cardsInPlay =[];
 	}
-	alert("Play again :)");
+	alert("Play again? :)");
 	//Use setTimeout to reload the page after 600 miliseconds
-	//Delay of 600ms so that the user can see the last card flipped.
 	//no idea what the bind is, copied from stackoverflow...
 	setTimeout(window.location.reload.bind(window.location), 600);
 };
@@ -36,47 +36,60 @@ var isMatch = function() {
 // Check if 2 cards are in play...
 var isTwoCards = function (event) {
 	// event and flipCard(event.target) idea taken from github examples.
-	// But is event needed, As it only is working on 'this' clicked card?
 	flipCard(event.target);
 	// Access the card clicked on
 	cardsInPlay.push(this.getAttribute('data-card'));
-	// add data-card to the cardsinplay array.
-
 	//If 2 cards are in play, chack for a match
 	if (cardsInPlay.length === 2) {
 		// Pass the cards to isMatch to see if they match
 		isMatch(cardsInPlay);
 		// Clear the cards for next time 
-		//cardsInPlay = [];
-	} else {		
+		cardsInPlay = [];
 	}
 };
 
 // Make the face of the cards appear
 function flipCard(card) {
-	if (card.getAttribute('data-card') === 'queen') {
-		card.innerHTML = '<img src="queen.png" alt="queen" />';
+	if (card.getAttribute('data-card') === 'queenH') {
+		card.innerHTML = '<img src="queenH.png" alt="queenhearts" />';
 		// cardElement.innerHTML = '<img src="my_king.png" alt="King of Spades" />';.
-	} else {
-		card.innerHTML = '<img src= "king.png" alt="king"/>';
+	} 
+	else if (card.getAttribute('data-card') === 'queenC') {
+		card.innerHTML = '<img src="queenC.png" alt="queenclubs" />';
+	}
+
+	else if (card.getAttribute('data-card') === 'queenS') { 
+		card.innerHTML = '<img src="queenS.png" alt="queenspades" />';
+	}
+
+	else if (card.getAttribute('data-card') === 'queenD') { 
+		card.innerHTML = '<img src="queenD.png" alt="queendiamonds" />';
+	}
+
+	else if (card.getAttribute('data-card') === 'kingH') {
+		card.innerHTML = '<img src="kingH.png" alt="kinghearts" />';
+	}
+
+	else if (card.getAttribute('data-card') === 'kingD') {
+		card.innerHTML = '<img src="kingD.png" alt="kingdiamonds" />';
+	}
+
+	else if (card.getAttribute('data-card') === 'kingC') {
+		card.innerHTML = '<img src="kingC.png" alt="kingdiamonds" />';
+	}
+
+	else {
+		card.innerHTML = '<img src= "kingS.png" alt="kingspades" />';
 	}
 };
+
 
 
 // Create the board, run the script to make the cards.
 createBoard();
 
-/*
-Improvements to make
-- When 2 cards not matching are flipped the pop-up happens before the face is seen.
-- The cards are only 2 types, ie. only queen of hearts. Would be more realistic with more suits
-- more cards
-- random order of cards on the board
-- more interesting background
-- more interesting game play, cards rotate slightly on flip etc.
-- Remove browser pop-ups and have them in the window
 
-*/
+
 
 /* Now Redundant
 for (var i=0; i<gameBoard.length; i++) {
